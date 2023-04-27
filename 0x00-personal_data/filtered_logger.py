1c6726f646efc415abf807f0f6f4a579cdd114c4
Reda <muhammadredabh@gmail.com>
2023-04-27 18:12:18 +0000

    fix styling

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,9 @@ from typing import List
 
 def filter_datum(fields: List[str],
                  redaction, message, separator):
-    """ function called filter_datum that returns
-    the log message obfuscated """
-    string = message
+    """ function filter_datum"""
     for el in fields:
         pattern = r"{}=.+?{}".format(el, separator)
-        string = re.sub(pattern, "{}={}{}".format(el,
-                                                  redaction,
-                                                  separator), string)
-
-    return string
+        message = re.sub(pattern,
+                        "{}={}{}".format(el, redaction, separator), message)
+    return message
