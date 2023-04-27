143e204111f81c993b6dd273f60b37da4e29c943
Reda <muhammadredabh@gmail.com>
2023-04-27 18:06:07 +0000

    separator case

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import re
 def filter_datum(fields, redaction, message, separator):
     string = message
     for el in fields:
-        pattern = r"({}=)[^{};]+".format(el, separator)
-        string = re.sub(pattern, r"\1{}".format(redaction), string)
+        pattern = r"{}=.+?{}".format(el, separator)
+        string = re.sub(pattern, "{}={}{}".format(el, redaction, separator), string)
 
     return string
