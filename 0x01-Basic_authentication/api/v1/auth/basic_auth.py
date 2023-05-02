54c6b8bb4793c305e543f9084d6a2246ceda5d69
Reda <muhammadredabh@gmail.com>
2023-05-02 18:37:33 +0100

    method documentation added

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,6 +21,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(self,
                                            base64_authorization_header: str
                                            ) -> str:
+        """decode_base64_authorization_header method"""
         if base64_authorization_header is None or \
                 not isinstance(base64_authorization_header, str):
             return None
