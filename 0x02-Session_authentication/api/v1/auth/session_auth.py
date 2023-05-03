f9d63b010efdd4fa6afa1973836cf310969fa2bb
Reda <muhammadredabh@gmail.com>
2023-05-03 14:50:04 +0100

    3. user_id_for_session_id method added - fixed uuid format

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,6 +18,14 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         else:
-            sessionId = uuid.uuid4()
+            sessionId = str(uuid.uuid4())
             self.user_id_by_session_id[sessionId] = user_id
             return sessionId
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """create_session method """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        else:
+            userId = self.user_id_by_session_id.get(session_id)
+            return userId
