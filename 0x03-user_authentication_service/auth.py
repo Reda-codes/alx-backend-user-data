c4736ee42808ac39dd9ae67fca4e994242cbe0b8
Reda <muhammadredabh@gmail.com>
2023-05-10 17:19:11 +0100

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,6 +86,5 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password)
-            return None
         except (Exception, NoResultFound, InvalidRequestError) as e:
             raise ValueError
