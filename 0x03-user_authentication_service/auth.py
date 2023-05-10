c22cfb27787ed5a6880b3338018c3f14078c35d7
Reda <muhammadredabh@gmail.com>
2023-05-10 17:16:44 +0100

    18. Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,13 @@ class Auth:
             return token
         except (Exception, NoResultFound, InvalidRequestError) as e:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Method to update a users's password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            return None
+        except Exception as e:
+            raise ValueError
