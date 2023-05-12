6736e9178f68710f676310ace7f9fb06a826758e
Reda <muhammadredabh@gmail.com>
2023-05-13 00:12:09 +0100

    main reset_password_token validation

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -58,7 +58,12 @@ def log_out(session_id: str) -> None:
 
 def reset_password_token(email: str) -> str:
     """ request a reset ppassword token """
-    pass
+    path = "{}/reset_password".format(url)
+    data = {"email": email}
+    response = requests.post(path, data)
+    assert response.status_code == 200
+    # print(response.json()["reset_token"])
+    return response.json()["reset_token"]
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
@@ -79,6 +84,6 @@ if __name__ == "__main__":
     session_id = log_in(EMAIL, PASSWD)
     profile_logged(session_id)
     log_out(session_id)
-    # reset_token = reset_password_token(EMAIL)
+    reset_token = reset_password_token(EMAIL)
     # update_password(EMAIL, reset_token, NEW_PASSWD)
     # log_in(EMAIL, NEW_PASSWD)
