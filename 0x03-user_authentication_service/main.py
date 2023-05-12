300fb2a9168de155744c4f8b36ee31b095513679
Reda <muhammadredabh@gmail.com>
2023-05-13 00:16:21 +0100

    main update_password validation

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -68,7 +68,13 @@ def reset_password_token(email: str) -> str:
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """ Update password """
-    pass
+    path = "{}/reset_password".format(url)
+    data = {"email": email,
+            "reset_token": reset_token,
+            "new_password": new_password}
+    response = requests.put(path, data)
+    assert response.status_code == 200
+    assert response.json()["message"] == "Password updated"
 
 
 EMAIL = "guillaume@holberton.io"
@@ -85,5 +91,5 @@ if __name__ == "__main__":
     profile_logged(session_id)
     log_out(session_id)
     reset_token = reset_password_token(EMAIL)
-    # update_password(EMAIL, reset_token, NEW_PASSWD)
-    # log_in(EMAIL, NEW_PASSWD)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
